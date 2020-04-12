41fdf067637f0669c6e532970627f2abe5da5f2f
Mayra Valdes <4valdes@gmail.com>
2020-04-12 17:30:10 -0500

    feat: add more cache files

--- scripts/ci/generate_tree.py
+++ scripts/ci/generate_tree.py
@@ -8,9 +8,15 @@ import sys
 import os
 import datetime
 from pathlib import Path
+from utils import *
+
 
 def main(args):
-
+    modified_df = pd.read_csv(
+        '{}/cache/meta/files.txt'.format(ROOT_DIR),
+        names=['Time', 'File']
+    ) 
+    
     df = pd.DataFrame(columns = ['File', 'Basename', 'Type', 'Subtype', 'Last Modified']) 
 
     for elem in Path('./www/').rglob('*.csv'):
@@ -20,7 +26,7 @@ def main(args):
             os.path.basename(elem).replace('.csv', ''),
             get_file_type(elem),
             get_file_subtype(elem),
-            get_last_modified(elem)
+            get_last_modified(elem, modified_df)
         ]
 
     df['Date'] = [get_file_date(x) for x in df.File]
@@ -28,11 +34,15 @@ def main(args):
     df['State'] = [get_state(x) for x in df.File]
     df['State_Name'] = [get_state_from_iso(x) for x in df.State]
 
-    print("Generating the meta files")
+    print('Generating the meta files')
+
     df.to_csv('./www/meta/files.csv', index=False)
     df.to_json('./www/meta/files.columns.json', orient="columns", indent=2)
     df.to_json('./www/meta/files.index.json', orient="index", indent=2)
     df.to_json('./www/meta/files.values.json', orient="values", indent=2)
+
+    # by group
+    df.groupby('Type').apply(lambda x: x.to_json('./www/meta/{}.json'.format(x.Type.iloc[0]), orient='records'))
 
 
 def get_file_type(elem):
@@ -51,7 +61,15 @@ def get_file_subtype(elem):
     return None
 
 
-def get_last_modified(elem):
+def get_last_modified(elem, df):
+    result = df[df['File'].str.contains(str(elem), regex=False, na=False)]
+
+    if len(result) == 0:
+        return result['Time'].values[0]
+    # if len(result) == 1:
+    #     return str(result['File'])
+    
+    # it works with local but not with CI
     return datetime.datetime.utcfromtimestamp(elem.stat().st_mtime).strftime('%Y-%m-%d %H:%M:%S')
 
 
---
+++ scripts/ci/utils.py
@@ -0,0 +1,14 @@
+import os
+
+#----------------------------------------------------------------
+# P r o j e c t
+#----------------------------------------------------------------
+
+ROOT_DIR = os.path.dirname(
+    os.path.dirname(os.path.dirname(os.path.abspath(__file__))
+)) # This is your Project Root
+CACHE_DIR = 'cache'
+
+URL_PDFS = 'https://www.gob.mx/salud/documentos/coronavirus-covid-19-comunicado-tecnico-diario-238449'
+URL_MAP = 'https://ncov.sinave.gob.mx/Mapa.aspx/Grafica22'
+
