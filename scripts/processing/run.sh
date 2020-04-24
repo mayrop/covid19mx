2dbe06708455c2f92268568ae0d38160ee6235f2
Mayra Valdes <4valdes@gmail.com>
2020-04-23 20:28:58 -0500

    ci: add logic to automate the last updated time

--- scripts/processing/run.sh
+++ scripts/processing/run.sh
@@ -3,6 +3,7 @@
 directory=$(cd `dirname $0` && pwd)
 root=$(dirname $directory)
 root=$(dirname $root)
+readme_file="${root}/README.md"
 
 data_dir="www"
 root_maps_dir="${root}/cache/mapa/"
@@ -13,7 +14,7 @@ root_timeseries_dir="${root}/${data_dir}/series-de-tiempo/federal/"
 echo "Moving to the root of the repo!"
 cd $root
 
-echo "Downloading files ;)"
+# echo "Downloading files ;)"
 python ./scripts/processing/download.py
 
 # processing each pdf file
@@ -37,6 +38,8 @@ do
         mv $source $target
         zip -9 -r $new_file $target
 
+        echo 
+
         echo "Removing temp files..."
         rm $file
         rm $target
@@ -47,6 +50,17 @@ do
     cd $root
 done
 
-# # generating the meta data files
+# generating the meta data files
 echo "Generating meta data files!"
 python ./scripts/processing/generate_meta_data.py 
+
+echo "Updating the last update time for open data!"
+last_update_od=$(find $root_zips_dir -iname \*.zip | sort -r | head -n 1 | sed -E "s@.*/([0-9]{4})([0-4]{2})([0-9]+).zip@\1-\2-\3@g")
+
+# spanish updates
+sed -ri -- "s@(<!-- UPDATE_ES:START.*>).*?(<!-- UPDATE_ES:END -->)@\1Última Actualización: ${last_update_od}\2@g" README.md
+sed -ri -- "s@(<!-- UPDATE-OPENDATA_ES:START.*>).*?(<!-- UPDATE-OPENDATA_ES:END -->)@\1Última Actualización: ${last_update_od}\2@g" README.md
+
+# english updates
+sed -ri -- "s@(<!-- UPDATE_EN:START.*>).*?(<!-- UPDATE_EN:END -->)@\1Last Update: ${last_update_od}\2@g" README.md
+sed -ri -- "s@(<!-- UPDATE-OPENDATA_EN:START.*>).*?(<!-- UPDATE-OPENDATA_EN:END -->)@\1Last Update: ${last_update_od}\2@g" README.md
